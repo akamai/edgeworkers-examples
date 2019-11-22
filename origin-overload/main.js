88ac5bd2f32dadca3a72425e26abe8193aec928e
Moritz <moritz.steiner@gmail.com>
2019-11-22 09:44:53 -0800

    Update main.js

--- origin-overload/main.js
+++ origin-overload/main.js
@@ -1,4 +1,11 @@
 /*
+(c) Copyright 2019 Akamai Technologies, Inc. Licensed under Apache 2 license.
+Version: 0.1
+Purpose:  Present a useful error page to the user, instead of a plain 503, and include js to automatically retry after the period indicated in the 'Retry-After' header. 
+Repo: https://github.com/akamai/edgeworkers-examples/tree/master/origin-overload
+*/
+
+/*
 onOriginResponse: This event happens as the origin response is created. 
 The event only happens if the response is not served from cache and not constructed on the edge. 
 Use this event if you want to modify the response before it is cached.

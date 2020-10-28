fb9b2ee46ac3197294732ff1e3424e9e004a905e
Steven Scian <sscian@akamai.com>
2020-10-28 11:52:40 -0400

    Update For Logging and Debugging.

--- hello-world/main.js
+++ hello-world/main.js
@@ -1,11 +1,25 @@
-// Hello World Example
+/*
+(c) Copyright 2020 Akamai Technologies, Inc. Licensed under Apache 2 license.
+
+Version: 0.2
+Purpose:  Modify an HTML streamed response by adding a script before the closing head tag.
+Repo: https://github.com/akamai/edgeworkers-examples/tree/master/hello-world
+*/
+
+// Import logging module
+import { logger } from 'log'
 
 export function onClientRequest(request) {
-  request.respondWith(
-      200, {},
-      '<html><body><h1>Hello World From Akamai EdgeWorkers</h1></body></html>');
+    // Outputs a message to the X-Akamai-EdgeWorker-onClientRequest-Log header.
+    logger.log("Responding with hello world from the path: %s", request.path)
+    request.respondWith(
+        200, {},
+        '<html><body><h1>Hello World From Akamai EdgeWorkers</h1></body></html>')
 }
 
 export function onClientResponse(request, response) {
-  response.setHeader('X-Hello-World', 'From Akamai EdgeWorkers');
+    // Outputs a message to the X-Akamai-EdgeWorker-onClientResponse-Log header.
+    logger.log("Adding a header in ClientResponse")
+
+    response.setHeader('X-Hello-World', 'From Akamai EdgeWorkers')
 }

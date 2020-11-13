2349c379b78624d3be3fd465a1dfb357c7dcdd38
Steven Scian <sscian@akamai.com>
2020-11-13 10:48:05 -0500

    ESLint code for consistency.

--- edgeworkers/libraries/find-replace-stream/main.js
+++ edgeworkers/libraries/find-replace-stream/main.js
@@ -12,21 +12,18 @@ import { createResponse } from 'create-response'
 import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform'
 import { FindAndReplaceStream } from 'find-replace-stream.js'
 
-export function responseProvider(request) {
+export function responseProvider (request) {
+  // Get text to be searched for and new replacement text from Property Manager variables in the request object.
+  const tosearchfor = request.getVariable('PMUSER_EWSEARCH')
+  const newtext = request.getVariable('PMUSER_EWNEWTEXT')
+  // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
+  const howManyReplacements = 3
 
-	//Get text to be searched for and new replacement text from Property Manager variables in the request object.
-	const tosearchfor =  request.getVariable('PMUSER_EWSEARCH');
-	const newtext =  request.getVariable('PMUSER_EWNEWTEXT');
-    //Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
-    const howManyReplacements = 3;
-    
-    return httpRequest(`${request.scheme}://${request.host}${request.url}`).then(response => {
-        return createResponse(
-            response.status,
-            response.headers,
-            response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new FindAndReplaceStream(tosearchfor, newtext, howManyReplacements)).pipeThrough(new TextEncoderStream())
-        )
-    })
+  return httpRequest(`${request.scheme}://${request.host}${request.url}`).then(response => {
+    return createResponse(
+      response.status,
+      response.headers,
+      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new FindAndReplaceStream(tosearchfor, newtext, howManyReplacements)).pipeThrough(new TextEncoderStream())
+    )
+  })
 }
-
-

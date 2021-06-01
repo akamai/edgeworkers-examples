8c798297b85c78201f30f743907cbd719422dd40
Łukasz Czerpak <lukasz@czerpak.eu>
2021-06-01 13:56:16 +0200

    added jsonp-wrapper example (#77)
    * added jsonp-wrapper example
    
    * Update edgeworkers/examples/respond-from-edgeworkers/responseprovider/jsonp-wrapper/README.md
    
    Co-authored-by: Ben Matthews <bmatthew@akamai.com>
    
    * Update edgeworkers/examples/respond-from-edgeworkers/responseprovider/jsonp-wrapper/README.md
    
    Co-authored-by: Ben Matthews <bmatthew@akamai.com>
    
    Co-authored-by: Ben Matthews <bmatthew@akamai.com>

---
+++ edgeworkers/examples/respond-from-edgeworkers/responseprovider/jsonp-wrapper/main.js
@@ -0,0 +1,36 @@
+import { TransformStream } from "streams";
+import { httpRequest } from "http-request";
+import { createResponse } from "create-response";
+import URLSearchParams from "url-search-params";  
+
+function str2uint8arr(s) {
+  return Uint8Array.from(Array.from(s, (x) => x.charCodeAt(0)))
+}
+
+export function responseProvider(request) {
+    let params = new URLSearchParams(request.query);
+    let callbackName = params.get("callback");
+    params.delete("callback");
+
+    const jsonpTransformer = new TransformStream({
+        transform(chunk, controller) {
+            if (chunk) {
+                controller.enqueue(chunk);
+            }
+        },
+        start(controller) {
+            controller.enqueue(str2uint8arr(callbackName + "("));
+        },
+        flush(controller) {
+            controller.enqueue(str2uint8arr(")"));
+        }
+    });
+  
+    return httpRequest(`${request.scheme}://${request.host}${request.path}?${params.toString()}`, options).then((response) => {
+        return createResponse(
+            response.status,
+            response.headers,
+            response.body.pipeThrough(jsonpTransformer)
+        );
+    });
+}

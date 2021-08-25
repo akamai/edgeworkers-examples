cdb03102a2a4e4677ad148b18a48c3d75c58cdca
fklasen <51907605+fklasen@users.noreply.github.com>
2021-08-25 19:37:16 +0200

    Update main.js

--- edgekv/examples/augment-image-manager-with-ew-edgekv/pristine-ew/main.js
+++ edgekv/examples/augment-image-manager-with-ew-edgekv/pristine-ew/main.js
@@ -1,4 +1,4 @@
-import {EdgeKV} from './edgekv.js';
+import {EdgeKV} from './edgekv.js'; //include this file from the parent repository. https://github.com/akamai/edgeworkers-examples/blob/master/edgekv/lib/edgekv.js
 import {logger} from 'log';
 export function onClientResponse(request, response) {
 	if (response.status === 200) {

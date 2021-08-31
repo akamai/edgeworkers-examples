0b5ff334a2641773e4bb9acc95210fbb8468de67
fklasen <51907605+fklasen@users.noreply.github.com>
2021-08-31 07:47:16 +0200

    Update main.js
    all change requests added

--- edgekv/examples/augment-image-manager-with-ew-edgekv/derivative-ew/main.js
+++ edgekv/examples/augment-image-manager-with-ew-edgekv/derivative-ew/main.js
@@ -1,17 +1,18 @@
-import {EdgeKV} from './edgekv.js'; //include this file from the parent repository. https://github.com/akamai/edgeworkers-examples/blob/master/edgekv/lib/edgekv.js
-import {logger} from 'log';
+import { EdgeKV } from "./edgekv.js"; //include this file from the parent repository. https://github.com/akamai/edgeworkers-examples/blob/master/edgekv/lib/edgekv.js
+import { logger } from "log";
+
+const edgeKv = new EdgeKV({ namespace: "im", group: "pristine" });
+
 export async function onClientRequest(request) {
-	var edgeKv = new EdgeKV({namespace: "im", group: "pristine"});
-	var size = 0;
-	try {
-		var key=request.getVariable('PMUSER_PATH_SHA1');
-	   	var value=await edgeKv.getText({item: key}); 
-	    if(value!==null){
-	      size = value;
-	    }
-	} 
-	catch (error) {
-		logger.log(`EKV: ${error.message} - Key is: ${key}`);
-	}
-	request.setVariable('PMUSER_PRISTINE_SIZE', size);
+  let size = 0;
+  try {
+    let key = request.getVariable("PMUSER_PATH_SHA1"); //this variable is set in Property Manager for the delivery configuration and contains the hashed URL, protocol+domain typically excluded
+    let value = await edgeKv.getText({ item: key });
+    if (value !== null) {
+      size = parseInt(value) || 0;
+    }
+  } catch (error) {
+    logger.log(`EKV: ${error.toString()} - Key is: ${key}`);
+  }
+  request.setVariable("PMUSER_PRISTINE_SIZE", size); //this is the variable that will be used by Image Manager, also defined in Property Manager
 }

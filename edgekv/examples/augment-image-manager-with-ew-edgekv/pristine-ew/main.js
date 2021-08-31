0cb817833824444523bf3586116467deff62cf37
fklasen <51907605+fklasen@users.noreply.github.com>
2021-08-31 07:47:40 +0200

    Update main.js
    all change requests added

--- edgekv/examples/augment-image-manager-with-ew-edgekv/pristine-ew/main.js
+++ edgekv/examples/augment-image-manager-with-ew-edgekv/pristine-ew/main.js
@@ -1,20 +1,22 @@
-import {EdgeKV} from './edgekv.js'; //include this file from the parent repository. https://github.com/akamai/edgeworkers-examples/blob/master/edgekv/lib/edgekv.js
-import {logger} from 'log';
+import { EdgeKV } from "./edgekv.js"; //include this file from the parent repository. https://github.com/akamai/edgeworkers-examples/blob/master/edgekv/lib/edgekv.js
+import { logger } from "log";
+
+const edgeKv = new EdgeKV({ namespace: "im", group: "pristine" });
+
 export function onClientResponse(request, response) {
-	if (response.status === 200) {
-    		var key=request.getVariable('PMUSER_PATH_SHA1');
-	    	
-		var size = 0;
-		var header = response.getHeader('Content-Length')
-		if (header) {
-		    size = header[0]
-		}  	
-		
-		var edgeKv = new EdgeKV({namespace: "im", group: "pristine"});
-     	
-		edgeKv.putText({item: key, value: size})
-    		.catch(
-			error => logger.log(`EKV: ${error.message}`)
-		);
-	}
+  if (response.status === 200) {
+    let key = request.getVariable("PMUSER_PATH_SHA1");
+    let size = 0;
+    let header = response.getHeader("Content-Length");
+
+    if (header) {
+      size = header[0];
+
+      try {
+        edgeKv.putText({ item: key, value: size });
+      } catch (error) {
+        logger.log(`EKV: ${error.toString()} - Key is: ${key}`);
+      }
+    }
+  }
 }

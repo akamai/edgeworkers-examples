12a2c4adb0dbf9b777fb6334512be354b38b3a6e
fklasen <51907605+fklasen@users.noreply.github.com>
2021-08-31 21:53:21 +0200

    Delete edgekv/examples/augment-image-manager-with-ew-edgekv directory

--- edgekv/examples/augment-image-manager-with-ew-edgekv/derivative-ew/edgekv_tokens.js
+++
@@ -1,7 +0,0 @@
-var edgekv_access_tokens = {
-        "namespace-default": {
-            "name": "default_token",
-            "value": "insert_customer_token_here"
-        }
-}
-export { edgekv_access_tokens };

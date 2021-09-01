9e6c05c5eae8f0d83933cb54b77a26c57569159f
fklasen <51907605+fklasen@users.noreply.github.com>
2021-09-01 15:54:41 +0200

    Add files via upload

---
+++ edgeworkers/examples/work-with-subrequests/slack-messenger/main.js
@@ -0,0 +1,18 @@
+import URLSearchParams from "url-search-params";
+import { httpRequest } from "http-request";
+
+export function onClientRequest(request) {
+  let params = new URLSearchParams(request.query);
+  let data =
+    '{"text":"' + (params.get("message") || "no message details") + '"}';
+  let url = `${request.scheme}://${request.host}/webhookpath/`;
+  try {
+    httpRequest(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    });
+  } catch (error) {
+    logger.log(`Error: ${error.toString()}`);
+  }
+}

2593813510fe9863cc1cd576b40cc9724c0f9e43
Tim Vereecke <58261095+tvereecke@users.noreply.github.com>
2023-03-08 20:29:45 +0100

    Cleaned up 103 Early Hints example

--- edgecompute/examples/103-early-hints/basic/main.js
+++ edgecompute/examples/103-early-hints/basic/main.js
@@ -1,5 +1,7 @@
-const CHROME_REGEX='Chrome\/([0-9]*).'; //Also matches Edge supporting 103 Early Hints
-const CHROME_MINVERSION=106; //Can be reduced
+const CHROME_REGEX='Chrome\/([0-9]*).'; //Regex to Extract Chrome version number
+const CHROME_MINVERSION=103; //103 Early hints support was added in Chrome 103
+const PMUSER_103_LIST='PMUSER_103_LIST';
+const PMUSER_103_ENABLED='PMUSER_103_ENABLED'; 
 const NAVIGATE='navigate'; 
 
 export async function onClientRequest(request){
@@ -9,11 +11,12 @@ export async function onClientRequest(request){
         hints.push('<https://cdn.example.com/assets/main.css>;rel=preload;as=style');
         hints.push('</assets/main.js>;rel=preload;as=script');
         hints.push('</fonts/my.woff2>;rel=preload;as=font;type=font/woff2;crossorigin');
-        request.setVariable('PMUSER_103_LIST',hints.join(',')); //max 1024-15=1009 bytes
-        request.setVariable('PMUSER_103_ENABLED',true);
+        request.setVariable(PMUSER_103_LIST,hints.join(',')); //max 1024-15=1009 bytes
+        request.setVariable(PMUSER_103_ENABLED,true);
     }
 }
 
+//Only run Early hints for supporting browsers (Chromium 103+) and when sec-fetch-mode=navigate
 function isEarlyHintAllowed(request){
     if(request.getHeader('sec-fetch-mode')[0] == NAVIGATE) {
         let match  = request.getHeader('user-agent')[0].match(CHROME_REGEX);
@@ -22,8 +25,4 @@ function isEarlyHintAllowed(request){
         }
     }
     return false;
-}
-
-export async function onClientResponse(request,response){
-    response.setHeader('X-103',request.getVariable('PMUSER_103_LIST'));
 }

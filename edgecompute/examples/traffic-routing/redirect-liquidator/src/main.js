09176f9d5ba87e010a95967e136840985ed94ea2
Tim Vereecke <58261095+tvereecke@users.noreply.github.com>
2023-05-23 14:29:38 +0200

    Update main.js

--- edgecompute/examples/traffic-routing/redirect-liquidator/src/main.js
+++ edgecompute/examples/traffic-routing/redirect-liquidator/src/main.js
@@ -1,15 +1,12 @@
 import { httpRequest } from 'http-request';
 import { createResponse } from 'create-response';
-import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform';
-import { FindAndReplaceStream } from 'find-replace-stream.js';
+import { HtmlRewritingStream } from 'html-rewriter';
+
 
 const REDIRECT_CODES=[301,302,303,307,308];
-const INJECT_AFTER_TAG = '</title>';
-// Some headers aren't safe to forward from the origin response through an EdgeWorker on to the client
-// For more information see the tech doc on create-response: https://techdocs.akamai.com/edgeworkers/docs/create-response
-const UNSAFE_RESPONSE_HEADERS = ['content-length', 'transfer-encoding', 'connection', 'vary',
-  'accept-encoding', 'content-encoding', 'keep-alive',
-  'proxy-authenticate', 'proxy-authorization', 'te', 'trailers', 'upgrade'];
+const INJECT_AFTER_TAG = 'title';
+const UNSAFE_RESPONSE_HEADERS=['content-length', 'transfer-encoding', 'connection', 'vary','accept-encoding', 
+'content-encoding', 'keep-alive','proxy-authenticate', 'proxy-authorization', 'te', 'trailers','transfer-encoding', 'upgrade'];
 
 export function responseProvider (request) {
   //Manage and calculate your Edge redirects without going to the origin. (Optional)
@@ -29,35 +26,37 @@ export function responseProvider (request) {
 function sendRecursiveOriginRequest(scheme,host,url,enableLiquidation){
     return httpRequest(`${scheme}://${host}${url}`).then(response => {
 
-        //Origin sent a redirect HTTP response status
-        if(REDIRECT_CODES.includes(response.status)){
-            let headers = response.getHeaders();
-            let locationHeader=headers["location"][0];
-            //Only chase the redirect when the location is on the same host and scheme.
-            if(locationHeader.startsWith(`${scheme}://${host}`)){
-                return sendRecursiveOriginRequest(scheme,host,locationHeader.replace(`${scheme}://${host}`,""),true);
-            }
-            else{
-                return sendOriginResponseAsIs(response);
-            }
+    //Origin sent a redirect HTTP response status
+    if(REDIRECT_CODES.includes(response.status)){
+        let headers = response.getHeaders();
+        let locationHeader=headers["location"][0];
+        //Only chase the redirect when the location is on the same host and scheme.
+        if(locationHeader.startsWith(`${scheme}://${host}`)){
+            return sendRecursiveOriginRequest(scheme,host,locationHeader.replace(`${scheme}://${host}`,""),true);
         }
-        //A redirect was previously detected and enableLiquidation turned on. The origin response body should be modified
-        else if(enableLiquidation){
-            const HISTORY_REPLACE_STATE = INJECT_AFTER_TAG+'<script>history.replaceState("","","'+url+'");</script>';
-            return createResponse(
-                response.status,
-                getSafeResponseHeaders(response.getHeaders()),
-                response.body
-                    .pipeThrough(new TextDecoderStream())
-                    .pipeThrough(new FindAndReplaceStream(INJECT_AFTER_TAG, HISTORY_REPLACE_STATE, 1))
-                    .pipeThrough(new TextEncoderStream())
-            );
-        }
-        //Nothing to do, sending the origin response as is
         else{
             return sendOriginResponseAsIs(response);
         }
-      });
+    }
+    //A redirect was previously detected and enableLiquidation turned on. The origin response body should be modified
+    else if(enableLiquidation){
+        let rewriter = new HtmlRewritingStream();
+
+        rewriter.onElement(INJECT_AFTER_TAG, el => {
+            el.after('<script>history.replaceState("","","'+url+'");</script>');
+            });
+
+        return createResponse(
+            response.status,
+            getSafeResponseHeaders(response.getHeaders()),
+            response.body.pipeThrough(rewriter) 
+        );
+    }
+    //Nothing to do, sending the origin response as is
+    else{
+        return sendOriginResponseAsIs(response);
+    }
+    });
 }
 
 //Send origin response as is without modifying the response body

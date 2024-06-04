f71bc26b424fb8bd1de39f804842b4324ea6c4e1
Tim Vereecke <58261095+tvereecke@users.noreply.github.com>
2024-06-04 15:23:26 +0200

    Update main.js for GA

--- edgecompute/examples/103-early-hints/dynamic-ekv/main.js
+++ edgecompute/examples/103-early-hints/dynamic-ekv/main.js
@@ -1,13 +1,13 @@
 import { EdgeKV } from './edgekv.js'; //You need to add the helper library https://techdocs.akamai.com/edgekv/docs/library-helper-methods
-const PMUSER_103_LIST='PMUSER_103_LIST';
+const PMUSER_103_HINTS='PMUSER_103_HINTS';
 const edgeKv = new EdgeKV({group: "earlyHints"});
 
 export async function onClientRequest(request){
     if(isEarlyHintsAllowed(request)){
       try {
-        let data = await edgeKv.getText({item:'103_list'});
+        let data = await edgeKv.getText({item:'103_hints'});
         if(data!=''){
-            request.setVariable(PMUSER_103_LIST,data); //Check size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
+            request.setVariable(PMUSER_103_HINTS,data); //Check size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
         }
       }
     }

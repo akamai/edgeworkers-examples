4b14562992b4eb876ad6744b6797ff06103870bb
Tim Vereecke <58261095+tvereecke@users.noreply.github.com>
2024-06-04 15:22:58 +0200

    Update main.js for GA

--- edgecompute/examples/103-early-hints/basic/main.js
+++ edgecompute/examples/103-early-hints/basic/main.js
@@ -1,4 +1,4 @@
-const PMUSER_103_LIST='PMUSER_103_LIST';
+const PMUSER_103_HINTS='PMUSER_103_HINTS';
 
 export async function onClientRequest(request){
         let hints = [];
@@ -6,5 +6,5 @@ export async function onClientRequest(request){
         hints.push('<https://cdn.example.com/assets/main.css>;rel=preload;as=style');
         hints.push('</assets/main.js>;rel=preload;as=script');
         hints.push('</fonts/my.woff2>;rel=preload;as=font;type=font/woff2;crossorigin');
-        request.setVariable(PMUSER_103_LIST,hints.join(',')); //Check variable size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
+        request.setVariable(PMUSER_103_HINTS,hints.join(',')); //Check variable size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
 }

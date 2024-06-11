e8b8026085041d244c37189e180b8127c47edbc8
Tim Vereecke <58261095+tvereecke@users.noreply.github.com>
2024-06-11 18:48:48 +0200

    Update main.js

--- edgecompute/examples/103-early-hints/dynamic/main.js
+++ edgecompute/examples/103-early-hints/dynamic/main.js
@@ -3,13 +3,11 @@ const PMUSER_103_HINTS='PMUSER_103_HINTS';
 const OPTIONS={timeout:50}; //Cancel Early hints when subrequest takes too long
 
 export async function onClientRequest(request){
-    if(isEarlyHintsAllowed(request)){
-      try {
-        let apiResponse = await httpRequest('https://www.yourdomain.com/api/listofresources',OPTIONS); //Cache+Extreme Prefresh for fast delivery and quick updates
-        if(apiResponse.status==200){
-            let data = await apiResponse.text();
-            request.setVariable(PMUSER_103_HINTS,data); //Check size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
-        }
-      }
+  try {
+    let apiResponse = await httpRequest('https://www.yourdomain.com/api/listofresources',OPTIONS); //Cache+Extreme Prefresh for fast delivery and quick updates
+    if(apiResponse.status==200){
+        let data = await apiResponse.text();
+        request.setVariable(PMUSER_103_HINTS,data); //Check size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
     }
+  }
 }

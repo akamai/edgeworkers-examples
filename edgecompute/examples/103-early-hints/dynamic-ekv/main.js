93afcfe963f327b463c2abb229eebc5c5d3be96f
Tim Vereecke <58261095+tvereecke@users.noreply.github.com>
2024-06-13 20:55:03 +0200

    Update main.js
    Removed reference to obsolete isEarlyHintsAllowed()

--- edgecompute/examples/103-early-hints/dynamic-ekv/main.js
+++ edgecompute/examples/103-early-hints/dynamic-ekv/main.js
@@ -3,12 +3,10 @@ const PMUSER_103_HINTS='PMUSER_103_HINTS';
 const edgeKv = new EdgeKV({group: "earlyHints"});
 
 export async function onClientRequest(request){
-    if(isEarlyHintsAllowed(request)){
       try {
         let data = await edgeKv.getText({item:'103_hints'});
         if(data!=''){
             request.setVariable(PMUSER_103_HINTS,data); //Check size limits https://techdocs.akamai.com/edgeworkers/docs/request-object#setvariable
         }
       }
-    }
 }

7ac983018d8e7111862feba05d9e5189286e2b15
Shushma44 <sushma.mududu434@gmail.com>
2024-08-02 16:19:19 +0530

    Add files via upload
    edgekv auto token renewal changes

---
+++ public_examples/edgekv-tokens.js
@@ -0,0 +1,10 @@
+
+
+ var edgekv_access_tokens = {
+  "namespace-test": {
+    "name": "my_auto_token_07252024",
+    "reference": "9e427ea3-33f4-5430-8794-22f1c21b1378",
+  }
+  };
+  export { edgekv_access_tokens };
+ 
---
+++ public_examples/main.js
@@ -0,0 +1,37 @@
+/*
+(c) Copyright 2019 Akamai Technologies, Inc. Licensed under Apache 2 license.
+Version: 0.1
+Purpose:  Respond with empty JSON if cart cookie not part of request.
+*/
+import { EdgeKV } from "./edgekv.js";
+import { Rule, defaultEngine, Operator } from "js-rules-engine";
+import { logger } from "log";
+
+init();
+
+
+function init() {
+  const caseInsensitivelyEquals = new Operator(
+    "caseInsensitivelyEquals",
+    function (a, b) {
+      return a.toUpperCase() === b.toUpperCase();
+    }
+  );
+  
+  defaultEngine.addOperator(caseInsensitivelyEquals);
+
+}
+
+
+export async function responseProvider(request) 
+{
+  //logger.log(`In responseProvider function request - ${JSON.stringify(decodeURI(request))}`);
+  try 
+  {
+    //let dccFeatureObj = JSON.parse(request.getVariable("PMUSER_DCC_CONFIG"));
+    const edgeKv = new EdgeKV({ namespace: "edgekv-token-test", group: "1111" });
+    logger.log('responseProvider function edgeKv: ', edgeKv);
+  }catch(error){
+    logger.log('responseProvider function error: ', error);
+  }
+}

7c47f9f2548005deb490c090863d53a61be8f4c1
hikaneko <hkaneko@akamai.com>
2024-10-16 08:08:38 +0900

    Update main.js

--- edgecompute/examples/traffic-filtering/block-based-on-ip/main.js
+++ edgecompute/examples/traffic-filtering/block-based-on-ip/main.js
@@ -1,9 +1,7 @@
 import { blockedIPs } from './ipList.js';
 
 export async function onClientRequest(request) {
-    const clientIP = request.getVariable('PMUSER_IP');
-
-    if (blockedIPs.includes(clientIP)) {
+    if (blockedIPs.includes(request.clientIp)) {
         request.respondWith(
             403, 
             { 'Content-Type': ['application/json;charset=utf-8'] }, 
